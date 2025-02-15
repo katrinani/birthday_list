a6cd2d5cf3090fea0c5de7a01060a7e43102f9aa
katrina <anisimovakaterina328@gmail.com>
2025-02-15 11:02:22 +0500

    Создание docker compose для базы данных и подключение к ней

--- db/db.go
+++ db/db.go
@@ -1,12 +1,20 @@
 package db
 
 import (
+	"baseToDo/dependencies"
+
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
 )
 
 func Connect() (*sql.DB, error) {
-	connStr := "user=- dbname=- sslmode=disable password=-"
+	connStr := fmt.Sprintf(
+		"user=%s dbname=%s sslmode=disable password=%s",
+		dependencies.UserDB,
+		dependencies.NameDB,
+		dependencies.PasswordDB,
+	)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
